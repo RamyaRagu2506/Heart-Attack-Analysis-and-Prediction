082ff25704762218337d99a6d081022fba2fd5f0
Srinivas <sm1043@gmail.com>
2022-07-30 19:09:51 +0200

    Changed chol cp comparison to a new version: ramya

--- rummuh.py
+++ rummuh.py
@@ -9,37 +9,39 @@ heart_data= pd.read_csv('heart.csv')
 #extract cholestrol data 
 heart_chol_data = heart_data.chol
 print(type(heart_chol_data))
+print(heart_chol_data)
 #plot chol data 
 index_chol = range(0,len(heart_chol_data))
-plt.scatter(index_chol, heart_chol_data)
-plt.ylabel("chol level")
-plt.show()
+#plt.scatter(index_chol, heart_chol_data)
+#plt.ylabel("chol level")
+#plt.show()
 
 #divide them into categories 
 chol_between_0_200 = heart_chol_data[np.logical_and(heart_chol_data >0,heart_chol_data<200)]
-print(np.mean(chol_between_0_200))
+#print(chol_between_0_200)
 
 chol_between_200_300 = heart_chol_data[np.logical_and(heart_chol_data > 200, heart_chol_data < 300)]
-print(len(chol_between_200_300))
+#print(len(chol_between_200_300))
 
 chol_above_300 = heart_chol_data[heart_chol_data > 300]
-print(np.mean(chol_above_300 ))
+#print(np.mean(chol_above_300 ))
 
 #Relate Chest pain type and cholestrol 
 chest_pain_type = heart_data[np.logical_and(heart_data.chol > 200, heart_data.chol < 300)]
 chest_pain_type = chest_pain_type.cp
 #print(chest_pain_type.head())
 compare_chol_between_200_300_chest_pain_type = pd.concat([chest_pain_type, chol_between_200_300], axis=1).reset_index()
-print(compare_chol_between_200_300_chest_pain_type.head())
 new_compare_data = compare_chol_between_200_300_chest_pain_type.drop(['index'],axis=1)
-print(len(new_compare_data))
+#print(len(new_compare_data))
+#print(new_compare_data.head())
 
 #grouping similar chol levels 
 group_cp_type = new_compare_data.groupby('cp').count()
-print(group_cp_type.columns, new_compare_data.columns)
+print(group_cp_type)
+print(group_cp_type.index)
 
 #plot the correlation
-plt.bar(new_compare_data.cp.unique(), group_cp_type.chol )
+plt.bar(group_cp_type.index, group_cp_type.chol )
 plt.ylabel("Chol levels")
 plt.xlabel("Chest pain type")
 plt.show()
